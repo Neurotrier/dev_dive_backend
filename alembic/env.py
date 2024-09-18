15d2905686025e3b05ce5832dcb1b6f0e4228012
AlekseyCherniaev <cherniaevaleksey2304@gmail.com>
2024-09-19 00:17:41 +0300

    [ENV-003]: Reinit alembic, changes in settings

--- alembic/env.py
+++ alembic/env.py
@@ -18,20 +18,16 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-
 target_metadata = Base.metadata
-
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 
 def run_migrations_offline() -> None:
