45a239dc39282f2842715a6ddb940e2243ab49dc
Neurotrier <neurotrier123@gmail.com>
2024-11-18 12:00:16 +0100

    [ENV-010]: Enable CORS

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.responses import RedirectResponse
 
 from src.api.v1.routes.router import router as router_v1
@@ -22,7 +23,13 @@ app = FastAPI(
     description=settings.PROJECT_DESCRIPTION,
 )
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router_v1)
 
 
