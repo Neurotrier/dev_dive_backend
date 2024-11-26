12c36e939be77123e8a6415bfd78475ea70793a2
Neurotrier <neurotrier123@gmail.com>
2024-11-26 11:19:42 +0100

    [ENV-011]: Update dockerization

--- workers.sh
+++ workers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-celery -A src.workers.celeryconfig worker --loglevel=info --pool=gevent --concurrency=4 -Q notification -n worker2 &
-celery -A src.workers.celeryconfig worker --loglevel=info --pool=gevent --concurrency=2 -n worker1 &
-celery -A src.workers.celeryconfig beat --loglevel=info
+uv run celery -A src.workers.celeryconfig worker --loglevel=info --pool=gevent --concurrency=4 -Q notification -n worker2 &
+uv run celery -A src.workers.celeryconfig worker --loglevel=info --pool=gevent --concurrency=2 -n worker1 &
+uv run celery -A src.workers.celeryconfig beat --loglevel=info
 
 
